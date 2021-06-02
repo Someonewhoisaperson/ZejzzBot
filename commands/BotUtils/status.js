c7a955555771c273ed77134fef0155abbd909428
Someonewhoisaperson <38829567+Someonewhoisaperson@users.noreply.github.com>
2021-06-02 14:52:59 +0100

    Move files into root directory to make automated actions easier

---
+++ commands/BotUtils/status.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-process-env */
+const Discord = require('discord.js');
+const prettyMilliseconds = require('pretty-ms');
+const { cpu } = require('node-os-utils');
+const os = require('os');
+const process = require('process');
+const { Logger } = require('../../common/common.js');
+const Log = new Logger();
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+	category: 'BotUtils',
+	name: 'status',
+	description: 'Shows status of the bot',
+	aliases: ['uptime', 'pingms'],
+	guildOnly: false,
+	botAdminOnly: true,
+	cooldown: 15,
+	minReqPermissions: [' '],
+	maxReqPermissions: [' '],
+	botExecutePermissions: ['SEND_MESSAGES'],
+	requireArgs: false,
+	stability: 'indev',
+	usage: 'status',
+	execute(client, message) {
+		// Create 3 embeds, one shows bot uptime, ping, api latency and downtime from discord
+		// Other shows connection to database and host computer Status
+		// Last one shows status of hypixel player tracker
+
+
+		message.reply('Checking status...').then(async (msg) => {
+			msg.delete();
+			cpu.usage().then(Cpuinfo => {
+				var dbTime1 = Date.now();
+				client.mysqlCon.query('SHOW TABLES', (err) => {
+					if (err) Log.Error(err);
+					var dbTimeDelta = Math.round(Date.now() - dbTime1);
+					const botStatusEmbed = new Discord.MessageEmbed()
+						.setColor('#0099ff')
+						.setTitle('Bot Status')
+						.setURL('https://dev.zejzz.net/')
+						.setDescription(`Command execution time: ${Math.round(Date.now() - message.createdTimestamp)}ms\n Ping:${msg.createdTimestamp - message.createdTimestamp}ms`)
+						.addFields(
+							{ name: '\u200B', value: '\u200B' },
+							{ name: 'Discord API Latency', value: `${Math.round(client.ws.ping)}ms`, inline: true },
+							{ name: 'Hypixel API Latency', value: `${Math.round(69420)}ms`, inline: true },
+							{ name: 'Reddit API Latency', value: `69420ms`, inline: true },
+							{ name: '\u200B', value: '\u200B' },
+							{ name: 'CPU Usage', value: `${Cpuinfo}% [${cpu.count()}]`, inline: true },
+							{ name: 'Memory usage', value: `${((os.totalmem / 1000000000) - (os.freemem / 1000000000)).toFixed(2)}gb/${(os.totalmem / 1000000000).toFixed(2)}gb`, inline: true },
+							{ name: 'Host Uptime', value: `${Math.round(os.uptime / 60 / 60)}h`, inline: true },
+							{ name: '\u200B', value: '\u200B' },
+							{ name: 'Proccess ID', value: process.pid, inline: true },
+							{ name: 'Node Version', value: process.version, inline: true },
+							{ name: 'Operating System', value: process.platform, inline: true },
+							{ name: '\u200B', value: '\u200B' },
+							{ name: 'Database Type', value: 'MySQL', inline: true },
+							{ name: 'Database size:', value: 'Error..', inline: true },
+							{ name: 'Database Latency', value: `${dbTimeDelta}ms`, inline: true }
+						)
+						.setTimestamp()
+						.setFooter(`Uptime: ${prettyMilliseconds(client.uptime)} | ${process.env.ENV_TYPE} build`);
+					message.channel.send(botStatusEmbed);
+				});
+			});
+		});
+		// TODO EMBED  OUT ACTIVE COMMANDS AND STATUS
+	}
+};
---
+++ events/commandExecuter.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-undef */
+/* eslint-disable complexity */
+/* eslint-disable consistent-return */
+const { Logger, config, countCharOccur } = require('../common/common.js');
+const Discord = require('discord.js');
+const Log = new Logger();
+const { prefix } = config;
+Log.debug(`commandExecuter.js listening for prefix : ${prefix}`);
+
+module.exports = {
+	eventType: 'message',
+	async: true,
+	once: false,
+	async execute(message, client) {
+		if (message.channel.type === 'dm') {
+			Log.verbose(`onMessage : ${message.content} ||| DirectMessage -> ${message.channel.id} -> ${message.id} <- ${message.author.id}`);
+		} else {
+			Log.verbose(`onMessage : ${message.content} ||| ${message.guild.id} -> ${message.channel.id} -> ${message.id} <- ${message.author.id}`);
+		}
+		// Bot ignores other bots, and messages without prefix
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+		// Cache sender (member) if not already
+		if (!message.member && !message.channel.type === 'dm') {
+			Log.debug(`${message.member} not cached`);
+			message.member = await message.guild.fetchMember(message);
+			Log.debug(`Cached user via message event : ${message.author.username}#${message.auther.discriminator} (${message.author.id}) `);
+		}
+
+		// Get args
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		Log.verbose(`Args : ${args} | ${message.id}`);
+		// Get command, is case insenitive
+		var cmdName = args.shift().toLowerCase();
+		Log.verbose(`cmdName: ${cmdName} | ${message.id}`);
+		// If command is empty then return
+		if (cmdName.length === 0) return;
+		// fetch commands from command collection
+
+		if (!client.commands.has(cmdName)) Log.verbose(`${cmdName} not found in client commands collection... checking sniper commands`);
+		global.commandaa = null;
+		// TODO , IF SNIPER COMMAND, MOVE ARGS DOWN, AND RUN ACTUAL COMMAND, AND CHECK FOR FOLDER INSIDE FOLDER OF SNIPER COMMAND!
+		if (cmdName === 's') {
+			cmdName = args.shift();
+			Log.debug(`New cmdName ${cmdName}`);
+			args.splice(0, 1);
+			Log.verbose(`Sniper command detected, new args ${args.shift()}`);
+			if (client.snipercommands.has(cmdName)) {
+				Log.verbose(`${cmdName} found in snipercommands`);
+				global.commandaa = client.snipercommands.get(cmdName);
+			} else {
+				message.reply('Unknown command');
+			}
+		}
+
+		if (!global.comandaa) global.commandaa = client.commands.get(cmdName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
+		const command = global.commandaa;
+		if (!command) {
+			Log.debug(`${cmdName} not found in client commands collection or aliases`);
+			return;
+		}
+		Log.debug(command.guildOnly);
+
+		if (command.guildOnly && message.channel.type === 'dm') return message.reply('Command can only be used in a server');
+
+
+		// Check user for permission to execute command
+		if (message.channel.type === '' && !message.member.hasPermission(command.minReqPermissions)) {
+			Log.verbose(`${message.author.id} did not have ${command.minReqPermissions} for ${command.name}`);
+			return message.reply('You do not have permission to use that command');
+		}
+
+		// Make sure we have the amount of arguments required
+		// Count how many required arguments are needed, by counting the amount of times '<' occurs in the usage and then dividing it by 2. Since each required argument is in the format <reqArg>
+		const requiredArgsAmount = countCharOccur(command.usage, '<');
+		Log.verbose(`Req args of ${command.name} are ${requiredArgsAmount} out of the arg length : ${args.length}`);
+		if (command.requireArgs && args.length < requiredArgsAmount) return message.reply(`Improper command usage! Missing required arguments. Correct usage: ${prefix}${command.usage}`);
+		const { cooldowns } = client;
+
+		if (!cooldowns.has(command.name)) {
+			Log.verbose(`${message.author.id} has no cooldown for  ${command.name}`);
+			cooldowns.set(command.name, new Discord.Collection());
+			Log.verbose(`${message.author.id} set cooldown for ${command.name}`);
+		}
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.name);
+		const cooldownAmount = (command.cooldown || 3) * 1000;
+		if (timestamps.has(message.author.id)) {
+			const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const timeLeft = (expirationTime - now) / 1000;
+				return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+			}
+		}
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+
+		if (command.botAdminOnly && !message.author.id === 717091956747927583) return message.reply('Denied.');
+		// Execute the command, if error then show it
+		if (command.stability === 'Dangerous' || command.stability === 'Caution') {
+			Log.warn(`${message.author.id} (${message.author.username}) attempted to run dangerous command ${command.name}`);
+			const cmdDangerEmbed = new Discord.MessageEmbed()
+				.setColor('e51d1d')
+				.setTitle('Potentially destructive command')
+				.setThumbnail('https://www.vkf-renzel.com/out/pictures/generated/product/1/650_650_75/r12044336-01/general-warning-sign-10836-1.jpg')
+				.setURL('https://dev.zejzz.net/')
+				.setDescription('This command has been flagged as being potentially dangerous to execute/reveals sensitive information. Make sure to remove/refractor this later')
+				.addFields({ name: command.description, value: `Warn Level: ${command.stability}` })
+				.setTimestamp();
+			message.channel.send(cmdDangerEmbed);
+		}
+		try {
+			command.execute(client, message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
+			Log.warn(`Error trying to execute cmdName: ${cmdName} | ${message.id} | ${message.author.id}`);
+		}
+	}
+};
